6fe66f01bbdb9df190e790de215b26645c2413f7
NinaWang20 <cnbjwzai@live.cn>
2018-12-04 09:03:41 +0800

    update

--- files/thank_you_/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,_(k,l,m,l),n,[o,p,q,r,s,t,u,v,w],x,_(y,z,A,B,C,D),E,_(m,_(y,z,A,B,C,D)),F,_(G,H,I,J,K,L,M,_(),N,_(O,P,Q,R,S,_(T,U,V,W),X,null,Y,R,Z,R,ba,bb,bc,null,bd,be,bf,bg,bh,bi,bj,be),bk,_(m,_()),bl,_(),bm,_(bn,[])),bo,_(),bp,_());}; 
-var b="url",c="thank_you_.html",d="generationDate",e=new Date(1542187513136.55),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="sketchKeys",k="",l="s0",m="a",n="variables",o="OnLoadVariable",p="TimeSec",q="TimeMin",r="zoomindex",s="moveYT",t="moveYD",u="moveXL",v="moveXR",w="randomtest",x="defaultBackgroundImageInfo",y="path",z="images/thank_you_/bg.png",A="width",B=299,C="height",D=38,E="viewIdToBackgroundImageInfo",F="page",G="packageId",H="fab8488667c445e5b6c9ad60e4c7deee",I="type",J="Axure:Page",K="name",L="Thank you!",M="notes",N="style",O="baseStyle",P="627587b6038d43cca051c114ac41ad32",Q="pageAlignment",R="center",S="fill",T="fillType",U="solid",V="color",W=0xFFFFFFFF,X="image",Y="imageHorizontalAlignment",Z="imageVerticalAlignment",ba="imageRepeat",bb="auto",bc="favicon",bd="sketchFactor",be="0",bf="colorStyle",bg="appliedColor",bh="fontName",bi="Applied Font",bj="borderWidth",bk="adaptiveStyles",bl="interactionMap",bm="diagram",bn="objects",bo="masters",bp="objectPaths";
-return _creator();
-})());
